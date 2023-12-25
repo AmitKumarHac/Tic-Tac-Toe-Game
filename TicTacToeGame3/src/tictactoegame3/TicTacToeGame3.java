3a424d34949bdcf9af07a03f100b0c4ffcc40d1b
Amit Kumar <130203377+AmitKumarHac@users.noreply.github.com>
2023-12-26 01:20:01 +0530

    Add files via upload

---
+++ TicTacToeGame3/src/tictactoegame3/TicTacToeGame3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tictactoegame3;
+
+/**
+ *
+ * @author 91912
+ */
+public class TicTacToeGame3 
+{
+    public static void main(String[] args) 
+    {
+       new UserPanel();
+    }
+    
+}
---
+++ TicTacToeGame3/src/tictactoegame3/WhoWinSeries.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tictactoegame3;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author 91912
+ */
+public class WhoWinSeries
+{
+
+    public WhoWinSeries(String message)
+    {
+        JFrame jf=new JFrame();
+        jf.setSize(600,400);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLayout(null);
+        
+        JLabel jl=new JLabel(message);
+        jl.setBounds(100, 100, 600, 50);
+        jl.setFont(new Font("Arial", 1, 30));
+        jl.setForeground(Color.red);
+        jf.add(jl);
+        
+        jf.setVisible(true);
+        
+    }
+    
+}
